246b1e53ed0382a6ebc76e43e582528b581534ec
Raja Sanwal Khan <43824565+RajaSanwal@users.noreply.github.com>
2022-09-18 18:06:26 +0500

    Update solution.py

--- Q1/solution.py
+++ Q1/solution.py
@@ -1,1 +1,25 @@
 ## Add code below with answer clearly stated
+def find_factorial(n, fact):
+    if n < 0:
+     print("Factorial does not exist for negative numbers")
+    elif n == 0:
+     print("The factorial of 0 is 1")
+    else:
+        for i in range(1,n + 1):
+            fact = fact*i
+    print("The factorial of",num,"is",fact)
+    return fact
+
+def sum_digits(output):
+    sum=0
+    my_list = [int(x) for x in str(output)]
+    for i in my_list:
+        sum +=i
+    return sum
+
+num = int(input("Enter a number: "))
+factorial = 1
+
+factorial_result = find_factorial(num,factorial)
+sum_result       = sum_digits(factorial_result)
+print("Sum of number is :", sum_result)
